6503a85a604fd0c6b5e3a583a6c9d3edb70b2ace
Palakaaran <67498221+Palakaaran@users.noreply.github.com>
2020-08-23 20:12:36 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -50,7 +50,7 @@ async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT,
-        reply_to_message_id=update.message_id
+        parse_mode="markdown"
     )
 
     await bot.send_message(
