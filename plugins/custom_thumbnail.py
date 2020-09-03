8f57aba83aa40e57c60e20eb7a1cb10fef1c9293
Palakaaran <67498221+Palakaaran@users.noreply.github.com>
2020-09-04 05:24:41 +0530

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -20,7 +20,7 @@ else:
     from config import Config
 
 # the Strings used for this "thing"
-from translation import Translation
+import Translation
 
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
