0a3828ff56a9d06e869d71a227cce62f5071a073
KL-35-Ronaldo-Fan <67498221+KL-35RonaldoFan@users.noreply.github.com>
2020-09-04 20:30:46 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -57,7 +57,7 @@ async def about_me(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.ABOUT_ME.format(
-            bot.full_name,
+            bot.first_name,
             bot.username
         ),
         parse_mode="html",
