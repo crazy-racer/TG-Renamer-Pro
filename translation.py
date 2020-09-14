19f45f4b4d010e6b9e80cfca0ddac4cb6db0b94b
unknown <67815810+unknown-01-hacker@users.noreply.github.com>
2020-09-14 16:58:22 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,32 +3,32 @@ class Translation(object):
 
 <b>എനിക്ക് ഏതെങ്കിലും ഒരു Telegram File Send ചെയ്യുക അതിന് Reply ആയി /rename NewName.extn എന്ന് തന്നാൽ ഞാൻ അത് Rename ചെയ്തു തരുന്നതായിരിക്കും.</b>
 
-<b>©️ @KL35RonaldoFan ©️ @KL35Cinemas ©️ @KL35Cinemaz</b>"""
+<b>©️ @KL_noob</b>"""
 
-    RENAME_403_ERR = "എന്തുവാടെ കാണിക്കുന്നേ? നിന്നെ ഞാൻ ban ചെയ്തിരിക്കുന്നു. \n\n<b>©  @KL35RonaldoFan ©️ @KL35Cinemas ©️ @KL35Cinemaz</b>"
-    ABS_TEXT = "നീ എന്തോ ചെയ്യാൻ നോക്കുവാടെ? \n\n<b>©  @KL35RonaldoFan ©️ @KL35Cinemas ©️ @KL35Cinemaz</b>"
-    UPGRADE_TEXT = "Contact: <b>@KL35RonaldoFan</b>"
-    DOWNLOAD_START = "ഞാൻ Download Start ചെയ്തിട്ടുണ്ട്. എനിക്ക് കുറച്ചു സമയം വേണം. \n\n<b>©  @KL35RonaldoFan ©️ @KL35Cinemas ©️ @KL35Cinemaz</b>"
-    UPLOAD_START = "ഹോ.. സമാധാനം Download ചെയ്ത് കഴിഞ്ഞു ഇനി Upload ചെയ്യണം. \n\n<b>©  @KL35RonaldoFan ©️ @KL35Cinemas ©️ @KL35Cinemaz</b>"
-    RCHD_TG_API_LIMIT = "നീ കണ്ടോ ഞാൻ {} സെക്കന്റിൽ Download ചെയ്തു.\nFile Size ഞാൻ കണ്ടുപിടിച്ചു: {}\nSorry നീ എന്തുവാ വിചാരിച്ചേ! ഞാൻ ഇപ്പോൾ Upload ചെയ്യും? \n\n<b>©  @KL35RonaldoFan ©️ @KL35Cinemas ©️ @KL35Cinemaz</b>"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Rename ചെയ്യാൻ എന്നെ ഉപയോഗിച്ചതിന് നന്ദി അളിയാ, നന്ദി > ©  @KL35RonaldoFan ©️ @KL35Cinemas ©️ @KL35Cinemaz**"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "നീ കണ്ടോ ഞാൻ {} സെക്കന്റിൽ Download ചെയ്തു.\nഎന്നിട്ട് {} സെക്കന്റിൽ Upload ചെയ്തു. \n\n<b>©  @KL35RonaldoFan ©️ @KL35Cinemas ©️ @KL35Cinemaz</b>"
-    NOT_AUTH_USER_TEXT = "Contact: <b>@KL35RonaldoFan</b>"
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "ഈ മെസ്സേജ് നിനക്ക് കിട്ടിയെങ്കിൽ നീ <b>@KL35RonaldoFan</b> മുതലാളിയെ Contact ചെയ്യണം."
-    REPLY_TO_DOC_FOR_RENAME_FILE = "ഒരു ഫയലിനു Reply ആയി <code>/rename</code> <b>NewName.ext</b> എന്ന് തരുക അപ്പോൾ ഞാൻ ആ File Rename ചെയ്തു തരുന്നതാണ്. \n\n<b>©  @KL35RonaldoFan ©️ @KL35Cinemas ©️ @KL35Cinemaz</b>"
-    SAVED_CUSTOM_THUMB_NAIL = "ഞാൻ Custom Thumbnail Save ചെയ്തു ✅. ഈ Thumbnail ആയിരിക്കും ഇനി മുതൽ ഉള്ള എല്ലാ ഫയലുകൾക്കും ഞാൻ Use ചെയ്യുക. \n\n<b>©  @KL35RonaldoFan ©️ @KL35Cinemas ©️ @KL35Cinemaz</b>"
-    DEL_ETED_CUSTOM_THUMB_NAIL = "ഞാൻ Custom Thumbnail Delete ചെയ്തു ❎️. ഇനി Thumbnail താരത്തെ അയക്കുന്ന ഫയലുകൾക്ക് Thumbnail കാണില്ല. \n\n<b>©  @KL35RonaldoFan ©️ @KL35Cinemas ©️ @KL35Cinemaz</b>"
+    RENAME_403_ERR = "എന്തുവാടെ കാണിക്കുന്നേ? നിന്നെ ഞാൻ ban ചെയ്തിരിക്കുന്നു. \n\n<b>©  @KL_noob</b>"
+    ABS_TEXT = "നീ എന്തോ ചെയ്യാൻ നോക്കുവാടെ? \n\n<b>©  @KL_noob</b>"
+    UPGRADE_TEXT = "Contact: <b>@KL_noob</b>"
+    DOWNLOAD_START = "ഞാൻ Download Start ചെയ്തിട്ടുണ്ട്. എനിക്ക് കുറച്ചു സമയം വേണം. \n\n<b>©  @KL_noob</b>"
+    UPLOAD_START = "ഹോ.. സമാധാനം Download ചെയ്ത് കഴിഞ്ഞു ഇനി Upload ചെയ്യണം. \n\n<b>©  @KL_noob</b>"
+    RCHD_TG_API_LIMIT = "നീ കണ്ടോ ഞാൻ {} സെക്കന്റിൽ Download ചെയ്തു.\nFile Size ഞാൻ കണ്ടുപിടിച്ചു: {}\nSorry നീ എന്തുവാ വിചാരിച്ചേ! ഞാൻ ഇപ്പോൾ Upload ചെയ്യും? \n\n<b>©  @KL_noob</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Rename ചെയ്യാൻ എന്നെ ഉപയോഗിച്ചതിന് നന്ദി അളിയാ, നന്ദി > ©  @KL_noob**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "നീ കണ്ടോ ഞാൻ {} സെക്കന്റിൽ Download ചെയ്തു.\nഎന്നിട്ട് {} സെക്കന്റിൽ Upload ചെയ്തു. \n\n<b>©  @KL_noob</b>"
+    NOT_AUTH_USER_TEXT = "Contact: <b>@KL_noob</b>"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "ഈ മെസ്സേജ് നിനക്ക് കിട്ടിയെങ്കിൽ നീ <b>@KL_noob</b> മുതലാളിയെ Contact ചെയ്യണം."
+    REPLY_TO_DOC_FOR_RENAME_FILE = "ഒരു ഫയലിനു Reply ആയി <code>/rename</code> <b>NewName.ext</b> എന്ന് തരുക അപ്പോൾ ഞാൻ ആ File Rename ചെയ്തു തരുന്നതാണ്. \n\n<b>©  @KL_noob</b>"
+    SAVED_CUSTOM_THUMB_NAIL = "ഞാൻ Custom Thumbnail Save ചെയ്തു ✅. ഈ Thumbnail ആയിരിക്കും ഇനി മുതൽ ഉള്ള എല്ലാ ഫയലുകൾക്കും ഞാൻ Use ചെയ്യുക. \n\n<b>©️ @KL_noob</b>"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "ഞാൻ Custom Thumbnail Delete ചെയ്തു ❎️. ഇനി Thumbnail താരത്തെ അയക്കുന്ന ഫയലുകൾക്ക് Thumbnail കാണില്ല. \n\n<b>©  @KL_noob</b>"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media ഞാൻ മൊത്തത്തിൽ അങ്ങ് കളഞ്ഞു."
-    SAVED_RECVD_DOC_FILE = "ഞാൻ File Download ചെയ്തു കഴിഞ്ഞു😎"
-    CUSTOM_CAPTION_UL_FILE = "<i><b>{}</b></i> \n<i><b>━━━━━━━━━ ✧ ━━━━━━━━</b></i> \n<i><b>💢💢</b></i> \n<i><b>━━━━━━━━━ ✧ ━━━━━━━━</b></i> \n\n<i><b>🎗ʝσιи🎗ѕнαяє🎗ѕυρρσят🎗</b></i>"
+    SAVED_RECVD_DOC_FILE = "ഞാൻ File Download ചെയ്തു കഴിഞ്ഞു🤓"
+    CUSTOM_CAPTION_UL_FILE = "<i><b>{}</b></i> \n<i><b> ✧━</b></i> \n<i><b>File_renamer_pro_bot</b></i> \n<i><b>━✧ </b></i> \n\n<i><b>ѕυρρσят🎗</b></i>"
     NO_CUSTOM_THUMB_NAIL_FOUND = "നീ എനിക്ക് Custom Thumbnail ഒന്നും തന്നില്ല. കഷ്ടം"
-    SHOW_CUSTOM_THUMB_NAIL = "<b>😎👆Permanent Thumbnail👆😎</b> \n\n<b>©  @KL35RonaldoFan ©️ @KL_noob ©️ @KL_noob</b>"
+    SHOW_CUSTOM_THUMB_NAIL = "<b>🔰Permanent Thumbnail🔰</b> \n\n<b>©️ @KL_noob</b>"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
-    KL35_THUMBNAIL_PHOTO = "https://telegra.ph/file/0b4e6202434fec2b29046.jpg"
-    ABOUT_ME = """<b>⭕️ Group: @KL35Cinemas</b> \n\n<b>⭕️ Channel: @KL35Cinemaz</b> \n\n<b>⭕️ Creator: @KL35RonaldoFan</b> \n\n<b>⭕️ Language: Python3</b> \n\n<b>⭕️ Library : <a href="https://docs.pyrogram.org/">Pyrogram asyncio 0.16.1</a></b> \n\n<b>⭕️ Source Code : 👉 <a href="https://bit.ly/2AVFsoD">Click Here</a></b> \n\n<b>©  @KL_noob</b>"""
+    KL35_THUMBNAIL_PHOTO = "https://telegra.ph/file/1ee9fd2c4b6a6b3c39262.jpg"
+    ABOUT_ME = """<b>⭕️ group:__</b> \n\n<b>➕ Name: @File_renamer_pro_bot</b> \n\n<b>➕ Creator: @KL_noob</b> \n\n<b>⭕️ Language: Python3</b> \n\n<b>⭕️ Library : <a href="https://docs.pyrogram.org/">Pyrogram asyncio 0.16.1</a></b> \n\n<b>⭕️ Source Code : 👉 <a href="https://bit.ly/2AVFsoD">Click Here</a></b> \n\n<b>©  @KL_noob</b>"""
     HELP_USER = """ഇത് അത്രക്ക് പാടൊന്നുമല്ലടെ.
     
 1. ഒരു Telegram File Send ചെയ്യുക.
 2. Thumbnail തരുക.(നീ ഒരു പ്രാവശ്യം തന്നാൽ മതി)
-3. ആ ഫയലിന് Reply ആയി /rename NewName.Extension എന്ന് തരുക. \n\n<b>©  @KL35RonaldoFan ©️ @KL35Cinemas ©️ @KL35Cinemaz</b>"""
+3. ആ ഫയലിന് Reply ആയി /rename NewName.Extension എന്ന് തരുക. \n\n<b>©  @KL_noob</b>"""
 
