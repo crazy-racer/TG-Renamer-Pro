f6237355d515ab4ddd6aa62a5819643718b5d825
unknown <67815810+unknown-01-hacker@users.noreply.github.com>
2020-09-14 17:55:58 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -63,8 +63,8 @@ async def help_user(bot, update):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('❤️My Group❤️', url="t.me/KL35Cinemas"),
-                    InlineKeyboardButton('💛My Channel💛', url="t.me/KL35Cinemaz")
+                    InlineKeyboardButton('🔰 creater', url="t.me/KL35Cinemas")
+                    
                 ],
                 [
                     InlineKeyboardButton('❓️Help❓️', callback_data="help_back"),
@@ -106,12 +106,12 @@ async def start(bot, update):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('📌Support📌', url="t.me/InFoTelGroup"),
+                    InlineKeyboardButton('📌Support📌', url="https://telegra.ph/file/1ee9fd2c4b6a6b3c39262.jpg"),
                     InlineKeyboardButton('🔥Share🔥', url="tg://msg?text=Hai%20Friend%20%E2%9D%A4%EF%B8%8F%2C%0AToday%20i%20just%20found%20out%20an%20intresting%20and%20Powerful%20%2A%2ARename%20Bot%2A%2A%20for%20Free%F0%9F%A5%B0.%20%0A%2A%2ABot%20Link%20%3A%2A%2A%20%40KL35RenamerBot%20%F0%9F%94%A5")
                 ],
                 [
-                    InlineKeyboardButton('❤️My Group❤️', url="t.me/KL35Cinemas"),
-                    InlineKeyboardButton('💛My Channel💛', url="t.me/KL35Cinemaz")
+                    InlineKeyboardButton('🔰 Creater', url="t.me/USER_IS_HACKER")
+                    
                 ],
                 [
                     InlineKeyboardButton('❓️Help❓️', callback_data="help_back"),
@@ -134,7 +134,7 @@ async def upgrade(bot, update):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('📌 Support', url="t.me/InFoTelGroup"),
+                    InlineKeyboardButton('📌 Support', url="t.me/0therGr0ups"),
                     InlineKeyboardButton('🔐Close🔐', callback_data="close")
                 ]
             ]
